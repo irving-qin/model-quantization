5218560feaa1b6d3d65ea20425b664665b718b47
chenp <chenp@own>
2020-07-15 12:08:50 +0930

    fix prone bn

--- models/resnet_.py
+++ models/resnet_.py
@@ -127,7 +127,7 @@ class BasicBlock(nn.Module):
             fconv3x3 = qprone
             sconv3x3 = qprone
 
-            if 'prone' in args.keyword and 'no_prone_downsample' in args.keyword and stride != 1 and keepdim:
+            if 'no_prone_downsample' in args.keyword and stride != 1 and keepdim:
                 fconv3x3 = conv3x3
 
             if 'preBN' in args.keyword:
@@ -135,9 +135,9 @@ class BasicBlock(nn.Module):
 
             if 'bn_before_restore' in args.keyword:
                 if fconv3x3 == qprone:
-                    self.bn1 = nn.ModuleList([nn.Sequential()])
+                    self.bn1 = nn.ModuleList([nn.Sequential() for j in range(args.base)])
                 if sconv3x3 == qprone:
-                    self.bn2 = nn.ModuleList([nn.Sequential()])
+                    self.bn2 = nn.ModuleList([nn.Sequential() for j in range(args.base)])
                    
             if stride != 1 and (args.input_size // feature_stride) % (2*stride) != 0:
                 extra_padding = ((2*stride) - ((args.input_size // feature_stride) % (2*stride))) // 2
@@ -295,18 +295,18 @@ class BottleNeck(nn.Module):
         extra_padding = 0
         # Prone network on
         if 'prone' in args.keyword:
-            keepdim = 'restore_shape' in args.keyword
             bn_before_restore = 'bn_before_restore' in args.keyword
             qconv3x3 = qprone
 
             if 'no_prone_downsample' in args.keyword and stride != 1 and keepdim:
                 qconv3x3 = conv3x3
 
-            if 'preBN' in args.keyword: # to be finished
+            if 'preBN' in args.keyword:
                 raise NotImplementedError("preBN not supported for the Prone yet")
-            else:
-                if not keepdim: # to be finished
-                    raise NotImplementedError("keepdim = False not supported for the Prone yet")
+
+            if 'bn_before_restore' in args.keyword:
+                if qconv3x3 == qprone:
+                    self.bn2 = nn.ModuleList([nn.Sequential() for j in range(args.base)])
 
             if stride != 1 and (args.input_size // feature_stride) % (2*stride) != 0:
                 extra_padding = ((2*stride) - ((args.input_size // feature_stride) % (2*stride))) // 2
