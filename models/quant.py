d01b224178f88cf5fd8cfb3d698bc05c8e7cedf2
gpu1 <mv@gpu1>
2021-05-19 18:50:47 +0930

    Fix bugs

--- models/quant.py
+++ models/quant.py
@@ -98,7 +98,10 @@ class quantization(nn.Module):
         if 'proxquant' in getattr(self.args, 'keyword', []):
             self.prox = 0
 
-        self.stable = getattr(args, 'stable', 0)
+	self.stable = getattr(args, self.tag + '_stable', 0)
+        if self.stable < 0:
+            self.stable = getattr(args, 'stable', 0)
+
         self.iteration = nn.Parameter(torch.zeros(1), requires_grad=False)
         self.level_num = nn.Parameter(torch.zeros(1), requires_grad=False)
         self.adaptive_restore = False
